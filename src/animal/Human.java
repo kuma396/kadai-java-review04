48724a117b040d6d08d36709d9f89c66935c62f8
kuma396 <k.shimo0330@gmail.com>
2025-02-17 01:49:15 +0900

    Lesson7の課題

---
+++ src/animal/Human.java
@@ -0,0 +1,28 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+
+    private String hobby;
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Human(String name, int age, String hobby) {
+        super(name, age);
+        super.setName(name);
+        super.setAge(age);
+        this.hobby = hobby;
+    }
+
+
+    public void think() {
+        System.out.println("私は" + hobby + "について考えています。");
+    }
+}
+
+
